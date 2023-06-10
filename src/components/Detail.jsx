4aa4eb2ba98f22d3af0703dc7caebebe2c002fbb
Burak Güllüler <burakgulluler@gmail.com>
2023-06-10 11:39:03 +0300

    detail component

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,17 +1,139 @@
-import React from 'react'
-import { styled } from 'styled-components'
+import React from "react";
+import { styled } from "styled-components";
 
 const Detail = () => {
   return (
     <Container>
-        Detail
+      <Background>
+        <img src="https://wallpaperaccess.com/full/288742.jpg" alt="" />
+      </Background>
+      <ImageTitle>
+        <img src="/images/bao-logo.png" alt="" />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" />
+          <span>TRAILER</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" />
+        </GroupWatchButton>
+      </Controls>
+      <SubTitle>2018 - 7m - Family, Fantasy, Kids, Animation</SubTitle>
+      <Describtion>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur vel
+        autem quisquam ducimus, incidunt, provident iste, alias sapiente culpa
+        quam quas rem hic dolor nihil blanditiis aliquam et minus. Soluta.
+      </Describtion>
     </Container>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
 
 const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vh + 5px);
+  position: relative;
+`;
 
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  width: 35vw;
+  min-height: 170px;
+  min-width: 200px;
+
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background-color: rgb(249, 249, 249);
+  border: none;
+  padding: 0 24px;
+  margin-right: 22px;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+const AddButton = styled.button`
+  margin-right: 16px;
+  min-width: 44px;
+  height: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+const GroupWatchButton = styled(AddButton)`
+  background-color: rgb(0, 0, 0);
+  max-width:44px;
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+
+`
+const Describtion = styled.div`
+  max-width: 800px;
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
 
 `
